05bff561104dee6b452970571cd3bdbed0aea37e
Vaishali <vaishalithakur614@gmail.com>
2020-11-03 22:28:13 +0530

    API Integration

--- src/Components/ViewOpportunity/Sections/Scholarships/ScholarshipsCard.js
+++ src/Components/ViewOpportunity/Sections/Scholarships/ScholarshipsCard.js
@@ -1,51 +1,65 @@
 import React from "react";
-
 import { Card, Row, Col, Container } from "react-bootstrap";
-import styles from "../../../../CSS/HackathonCard.module.css";
+import logo from "../../../../Assets/amdocs.png"
+import styles from "../../../../CSS/Scholarship.module.css";
 
 function ScholarshipsCard(props) {
+  const { item } = props;
+
   return (
-    <div id="scholarship" className={styles.Box}>
-      <Card className={styles.Card}>
+    <div id = "scholarship" className = {styles.Box}>
+      <Card className = {styles.Card}>
         <Row>
           <Card.Img
-            variant="top"
-            src={require("../../../../Assets/" + props.knowAbout.imgUrl)}
-            alt="Image"
-            className={styles.CardImage}
+            variant = "top"
+            src = {logo}
+            // src = {item.imgURL}            
+            alt = {item.title}
+            className = {styles.CardImage}
           />
-          <h1 className={styles.Title} style={{ color: "blue" }}>
-            TITLE
+          <h1 className = {styles.Title}>
+            {item.title}
           </h1>
         </Row>
         <Row>
-          <h6 className={styles.Description}>{props.knowAbout.description}</h6>
+          <p className = {styles.Description}>
+            {item.description}
+          </p>
         </Row>
         <Container>
           <Row>
-            <Col md={8} lg={4}>
+            <Col md = {12} lg = {12}>
               <Row>
-                <h5 className={styles.Heading} style={{ marginLeft: "20px" }}>
-                  Deadline :{" "}
+                <h5 className = {styles.Heading} style = {{ marginLeft: "20px" }}>
+                  Deadline :
                 </h5>
-                <h5 className={styles.Value}>{props.knowAbout.deadline}</h5>
+                <h5 className = {styles.Value}>
+                  {item.deadline}
+                </h5>
               </Row>
             </Col>
           </Row>
           <Row>
-            <Col md={8} lg={4}>
+            <Col md = {12} lg = {12}>
               <Row>
-                <h5 className={styles.Heading} style={{ marginLeft: "20px" }}>
-                  {" "}
-                  Eligibility:{" "}
-                </h5>
-                <h5 className={styles.Value}>{props.knowAbout.eligibility}</h5>
+                <Container>
+                  <h5 className = {styles.Heading} style = {{ marginLeft: "5px" }}>
+                    Eligibility : 
+                    <span className = {styles.Value}>
+                      {item.eligibility} 
+                    </span>
+                  </h5>
+                </Container>
               </Row>
             </Col>
           </Row>
           <Row>
-            <Col md={0} lg={12}>
-              <button className={styles.Button}>Apply</button>
+            <Col md = {0} lg = {12}>
+              <a href = {item.url}>
+                <button className = {styles.Button}>
+                  Apply
+                </button>
+              </a>
             </Col>
           </Row>
         </Container>
