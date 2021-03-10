fa1f03df6c2267a496dce10194ceb1233fe38069
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-03-10 16:04:27 +0530

    Update KnowAbout.js
    #41

--- src/Components/HomePage/Sections/KnowAbout.js
+++ src/Components/HomePage/Sections/KnowAbout.js
@@ -3,6 +3,51 @@ import KnowAboutCard from './KnowAboutCard';
 import KnowAboutData from './KnowAboutData';
 import { Container, Jumbotron, Row } from 'react-bootstrap';
 import styles from '../../../CSS/KnowAbout.module.css';
+import React, { Component } from 'react';
+import KnowAboutCard from './KnowAboutCard';
+import KnowAboutData from './KnowAboutData';
+import { Container, Jumbotron, Row } from 'react-bootstrap';
+import styles from '../../../CSS/KnowAbout.module.css';
+
+class KnowAbout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      KnowAboutData,
+    };
+  }
+
+  render() {
+    let KnowAboutData = this.state.KnowAboutData.map((knowAbout) => {
+      return (
+        <KnowAboutCard
+          key={knowAbout.id}
+          knowAbout={knowAbout}
+          link={KnowAbout.link}
+        />
+      );
+    });
+
+    return (
+      <Jumbotron  className={styles.Jumbotron}>
+        <Container fluid="sm">
+          <h2 id = 'about' className={styles.Heading}>
+            Know
+            <span className={styles.About}> About </span>
+          </h2>
+          <Row
+            className="justify-content-md-center"
+            style={{ marginTop: '50px' }}
+          >
+            {KnowAboutData}
+          </Row>
+        </Container>
+      </Jumbotron>
+    );
+  }
+}
+
+export default KnowAbout;
 
 class KnowAbout extends Component {
   constructor() {
