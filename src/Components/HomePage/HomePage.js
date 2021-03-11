93a669c5604cc6e59aec8bf1444bf7cba381edd2
SpecTEviL <vishalpatil.imp@gmail.com>
2021-03-11 11:35:21 +0530

    (feature/AboutUs) - Added AboutUs component

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './Sections/Navbar';
 import Banner from './Sections/Banner';
 import KnowAbout from './Sections/KnowAbout';
+import AboutUs from "./Sections/AboutUs";
 
 class HomePage extends Component {
   render() {
@@ -9,6 +10,7 @@ class HomePage extends Component {
       <div>
         <Navbar />
         <Banner />
+        <AboutUs/>
         <KnowAbout />
       </div>
     );
