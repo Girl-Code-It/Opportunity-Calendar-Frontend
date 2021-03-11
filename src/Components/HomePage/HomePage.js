3446e01228ba3381560e100c335b45a68cf86c8e
khushboog12 <65533036+khushboog12@users.noreply.github.com>
2021-03-11 22:55:54 +0530

    (feature/Contact Us) - Added Contact Us component to homepage. (#83)
    Co-authored-by: Abdus <abdus@abdus.net>

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -3,7 +3,9 @@ import Navbar from './Sections/Navbar';
 import Banner from './Sections/Banner';
 import KnowAbout from './Sections/KnowAbout';
 import Faq from './Sections/FAQ_main';
-import AboutUs from './Sections/AboutUs';
+import AboutUs from "./Sections/AboutUs"; 
+import ContactUs from "./Sections/ContactUs"; 
+
 
 class HomePage extends Component {
   render() {
@@ -11,9 +13,10 @@ class HomePage extends Component {
       <div>
         <Navbar />
         <Banner />
-        <AboutUs />
+        <AboutUs/>
         <KnowAbout />
-        <Faq />
+	      <Faq/>
+        <ContactUs />
       </div>
     );
   }
