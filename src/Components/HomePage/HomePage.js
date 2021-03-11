1e63bd48caa542dbf9b53adbf5c888435ad59dde
Vishal Patil <vishalpatil.imp@gmail.com>
2021-03-11 11:57:43 +0530

    Update HomePage.js
    Resolved the merge conflict of Faq component.

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Navbar from './Sections/Navbar';
 import Banner from './Sections/Banner';
 import KnowAbout from './Sections/KnowAbout';
+import Faq from './Sections/FAQ_main';
 import AboutUs from "./Sections/AboutUs";
+
 
 class HomePage extends Component {
   render() {
@@ -12,6 +14,7 @@ class HomePage extends Component {
         <Banner />
         <AboutUs/>
         <KnowAbout />
+        <Faq/>
       </div>
     );
   }
