970114d01cabe81953d3cc424070a2089982ccc2
ANAS RIYAZ <anasriyaz92@gmail.com>
2021-03-13 00:18:14 +0530

    feature/Enable Contact-us form submissions through Formspree API

--- src/Components/HomePage/Sections/ContactUs.js
+++ src/Components/HomePage/Sections/ContactUs.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import {
   Jumbotron,
   Container,
@@ -10,61 +11,99 @@ import {
 import styles from "../../../CSS/ContactUs.module.css";
 
 export default function ContactUs() {
-    return (
-      <Jumbotron
-        id="contact"
-        style={{ paddingTop: "0px", backgroundColor: "#ffffff" }}
-      >
-        <Container fluid="sm">
-          <h1 className={styles.Heading}>
-            Get In <span style={{ color: "#008dc8" }}>Touch</span>
-          </h1>
-          <Row
-            className="justify-content-md-center"
-            style={{ marginTop: "50px" }}
-          >
-            <Col lg={5}>
-              <Form >
-                <Form.Group>
-                  <Form.Control
-                    type="text"
-                    name="name"
-                    defaultValue=""
-                    placeholder="John Doe"
-                  />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    defaultValue=""
-                    placeholder="john@example.com"
-                    required
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Control
-                    as="textarea"
-                    rows="3"
-                    name="message"
-                    defaultValue=""
-                    placeholder="Message"
-                    required
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Button
-                
-                    className={styles.Button}
-                    type="submit"
-                  >
-                    Send Message
-                  </Button>
-                </Form.Group>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      </Jumbotron>
-    );
+
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null
+  })
+
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg }
+    });
+    if (ok) {
+      form.reset();
+    }
   }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    setServerState({ submitting: true });
+    axios({
+      method: "post",
+      url: 'https://formspree.io/' + process.env.FORMSPREE_API_KEY,
+      data: new FormData(form)
+    })
+      .then(r => {
+        handleServerResponse(true, "Thanks!", form);
+      })
+      .catch(r => {
+        handleServerResponse(false, r.response.data.error, form);
+      });
+  }
+
+  return (
+    <Jumbotron
+      id="contact"
+      style={{ paddingTop: "0px", backgroundColor: "#ffffff" }}
+    >
+      <Container fluid="sm">
+        <h1 className={styles.Heading}>
+          Get In <span style={{ color: "#008dc8" }}>Touch</span>
+        </h1>
+        <Row
+          className="justify-content-md-center"
+          style={{ marginTop: "50px" }}
+        >
+          <Col lg={5}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  defaultValue=""
+                  placeholder="John Doe"
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control
+                  type="email"
+                  name="email"
+                  defaultValue=""
+                  placeholder="john@example.com"
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  as="textarea"
+                  rows="5"
+                  name="message"
+                  defaultValue=""
+                  placeholder="Message"
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Button
+
+                  className={styles.Button}
+                  type="submit"
+                >
+                  Send Message
+                  </Button>
+                {serverState.status && (
+                  <p className={!serverState.status.ok ? "Error Msg" : ""}>
+                    {serverState.status.msg}
+                  </p>
+                )}
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
+}
