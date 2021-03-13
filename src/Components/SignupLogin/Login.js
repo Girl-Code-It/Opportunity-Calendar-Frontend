dbf10569991a476ad5eaa4a6e805c3cdb19dab90
Sushi22 <sushmitajoshi22@gmail.com>
2021-03-13 22:57:35 +0530

    button styled

--- src/Components/SignupLogin/Login.js
+++ src/Components/SignupLogin/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from "../../../CSS/SignupLogin.module.css";
+import styles from "../../CSS/SignupLogin.module.css";
 import { Form,Card,Button, Row} from 'react-bootstrap';
 import { GoogleLogin } from 'react-google-login';
 
 const Login = ()=>{
     return(
-        <Row className="justify-content-md-center">
-            <Card className={styles.card}>
+    <Row className="justify-content-md-center">
+        <Card className={styles.card}>
             <h5>Login</h5>
             <Form>
                 <Form.Group controlId="email">
@@ -17,15 +17,15 @@ const Login = ()=>{
                 <Form.Group controlId="passWord">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password"/>
-                    <a href="#"><small className={styles.link}>Forgot password ?</small></a>
+                    <a href="#"><small >Forgot password ?</small></a>
                 </Form.Group>
             </Form>
-            <Button size="md" className="button">Login</Button>
+            <Button size="md" className={styles.button}>Login</Button>
                 <p>OR</p>
                 <GoogleLogin
                     buttonText="Login with Google"
                 />
-                <p>Don't have an Account ? <a href="/signup" className={styles.link}>Signup</a></p>
+                <p>Don't have an Account ? <a href="/signup" >Signup</a></p>
         </Card>
     </Row> 
     )
