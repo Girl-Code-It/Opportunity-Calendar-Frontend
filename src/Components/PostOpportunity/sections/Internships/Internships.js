32f5e0a6c8ace6b29a32622f2bbf783da654eb38
girkarkshitij <girkarkshitij@gmail.com>
2021-03-14 18:42:29 +0530

    Fixes #95 - Changed stateless class components to functions

--- src/Components/PostOpportunity/sections/Internships/Internships.js
+++ src/Components/PostOpportunity/sections/Internships/Internships.js
@@ -3,17 +3,15 @@ import Navbar from '../Navbar';
 import InternshipForm from './InternshipForm';
 import styles from '../../../../CSS/FullTimeForm.module.css';
 
-class FullTime extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div className={styles.FullTimeComponent}>
-          <InternshipForm />
-        </div>
+function FullTime() {
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.FullTimeComponent}>
+        <InternshipForm />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default FullTime;
