b52e42499c28f1baf25c37c0fbafc03450ecd81b
Shreyash Lata <shreyeslata@gmail.com>
2021-03-24 01:28:46 +0530

    Modify About Us Content

--- src/Components/HomePage/Sections/AboutUs.js
+++ src/Components/HomePage/Sections/AboutUs.js
@@ -21,24 +21,22 @@ export default function AboutUs() {
           </Col>
           <Col md={'auto'} lg={5}>
             <p className={styles.AboutUs_description}>
-              <i>"Knowledge is the only thing that grows when shared." </i>
               <br />
               <br />
-              Girl Code It aka GCI, is a group of people who firmly believe in
-              this statement and are eager to build a healthy coding culture
-              among young coders.
+              <br />
+              If you feel like you keep missing on good opportunities that others grab
+              easily only because you never find out about them in time, we got you covered!!
               <br />
               <br />
-              The best thing about the internet is that there is no shortage of
-              information; but sometimes, there is just too much information,
-              which can be overwhelming. To streamline the amount of information
-              in the right way can be a taxing process, but here is where we can
-              be of assistance! Our codemaps are specially structured according
-              to common needs and are easy to follow! All you need to bring with
-              you is the will to learn!
+              <br />
+              Opportunity Calendar provides instant information on newly posted opportunities for techies.
+              Now you won't miss another deadline for applying to your dream companies.
+              The calendar is your one-stop solution to know all about the latest Job openings,
+              Internships, Scholarships, and all tech events including Tech Conferences,
+              Hackathons, and other Coding Competitions.
               <br />
               <br />
-              So what are you waiting for? Come join us!
+
             </p>
             <br />
             <br />
